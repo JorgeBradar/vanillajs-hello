868920d35a7349523a5b3bfd5cab813d4938435b
Jorge Arias <96737213+JorgeBradar@users.noreply.github.com>
2024-01-20 16:42:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let text = "";
+  let excuse = document.getElementById("excuse");
+
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+
+  let what = ["my homework ", "the keys ", "the car "];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  text += who[Math.trunc(Math.random() * 3)];
+  text += action[Math.trunc(Math.random() * 3)];
+  text += what[Math.trunc(Math.random() * 2)];
+  text += when[Math.trunc(Math.random() * 4)];
+
+  excuse.textContent = text;
+  excuse.style.fontSize = "150px";
+  excuse.style.textAlign = "center";
 };
